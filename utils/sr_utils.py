d0bed35fabb69fa289cfa5f692eb8c77cb4bea56
cclauss <cclauss@bluewin.ch>
2017-12-02 17:05:34 -0500

    print() is a function in Python 3

--- utils/sr_utils.py
+++ utils/sr_utils.py
@@ -50,7 +50,7 @@ def load_LR_HR_imgs_sr(fname, imsize, factor, enforse_div32=None):
     img_LR_pil = img_HR_pil.resize(LR_size, Image.ANTIALIAS)
     img_LR_np = pil_to_np(img_LR_pil)
 
-    print 'HR and LR resolutions: %s, %s' % (str(img_HR_pil.size), str (img_LR_pil.size))
+    print('HR and LR resolutions: %s, %s' % (str(img_HR_pil.size), str (img_LR_pil.size)))
     # else:
     #     assert False
 
