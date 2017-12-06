8067fbbae78637e743ce40f362d0bdfdebfcd6ad
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2017-12-07 00:47:54 +0300

    fix plot_image_grid

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -69,6 +69,11 @@ def plot_image_grid(images_np, nrow =8, factor=1, interpolation=None):
         factor: size if the plt.figure 
         interpolation: interpolation used in plt.imshow
     """
+    n_channels = max(x.shape[0] for x in images_np)
+    assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
+    
+    images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
+
     grid = get_image_grid(images_np, nrow)
     
     plt.figure(figsize=(len(images_np)+factor,12+factor))
